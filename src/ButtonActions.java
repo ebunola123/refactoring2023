8b6be1eb83baa2f1e27923bc4731269ce736e4ec
Ebunola Ajayi <c19324126@mytudublin.ie>
2023-03-05 23:25:25 +0000

    first commit - ButtonActions.java

---
+++ src/ButtonActions.java
@@ -0,0 +1,19 @@
+
+public interface ButtonActions {
+	
+
+	public void exitApp();
+	public void openFile();
+	public void saveFile();
+	public void saveFileAs();
+	public void displaySearchByIdDialog();
+	public void displaySearchBySurnameDialog();
+	public void searchEmployeeById();
+	public void searchEmployeeBySurname();
+	public void cancelChange();
+	public void displayRecords();
+	public void displayEmployeeSummaryDialog();
+	public void editDetails();
+	public void deleteRecord();
+
+}
